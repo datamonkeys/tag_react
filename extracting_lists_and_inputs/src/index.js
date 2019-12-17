24986a1f731d5546c7fe76ba055fd23427cd2a60
datamonkeys.ai <57781209+datamonkeys@users.noreply.github.com>
2019-12-17 17:09:49 +0100

    Update index.js

--- extracting_lists_and_inputs/src/index.js
+++ extracting_lists_and_inputs/src/index.js
@@ -2,83 +2,56 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-class Inputs extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      names: [],
-      firstNameInput: "",
-      lastNameInput: ""
+      quotes: false
     };
   }
 
-  handleFirstNameInput = event => {
-    this.setState({
-      firstNameInput: event.target.value
-    });
-  };
-
-  handleLastNameInput = event => {
-    this.setState({
-      lastNameInput: event.target.value
-    });
-  };
-
-  handleClick = () => {
-    // get data from state
-    const fullName = `${this.state.firstNameInput}  ${this.state.lastNameInput}`;
-
-    // get names array and push data to it
-    const names = this.state.names;
-    names.push(fullName);
-
-    // set it to state
-    this.setState({
-      names,
-      firstNameInput: "",
-      lastNameInput: ""
-    });
-  };
+  componentDidMount() {
+    fetch("https://programming-quotes-api.herokuapp.com/quotes/lang/en")
+      .then(response => response.json())
+      .then(resp => {
+        this.setState({
+          quotes: resp
+        });
+      });
+  }
 
   render() {
     return (
       <div>
-        <input
-          type="text"
-          value={this.state.firstNameInput}
-          onChange={this.handleFirstNameInput}
-          placeholder="First"
-        />
-        <input
-          type="text"
-          value={this.state.lastNameInput}
-          onChange={this.handleLastNameInput}
-          placeholder="Last"
-        />
-        <button onClick={this.handleClick}>Add</button>
-        <Lists names={this.state.names} />
+        {this.state.quotes && <ListContainer quotes={this.state.quotes} />}
       </div>
     );
   }
 }
 
 const Lists = props => {
-  const names = props.names; // Here we get our names array which we want to display
+  const quotes = props.quotes;
 
-  // We iterate over the array of names and create a ListItem do be displayed
-  const listItems = names.map((name, index) => (
-    <ListItem key={index} name={name} />
+  const listItems = quotes.map(quote => (
+    <ListItem key={quote.id} author={quote.author} quote={quote.en} />
   ));
   return <ul>{listItems}</ul>;
 };
 
 const ListItem = props => {
-  const { name } = props; //We deconstruct the object here to get our name
+  const { quote, author } = props;
   return (
     <li>
-      <span>{name}</span>
+      <span>{quote}</span>
+      <span>
+        {""} - <strong>{author}</strong>
+      </span>
     </li>
   );
 };
 
-ReactDOM.render(<Inputs />, document.getElementById("root"));
+const ListContainer = props => {
+  return <Lists quotes={props.quotes} />;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
